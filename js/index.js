0a1b346ea73ea707c99430464a8d7ee796e6a175
santiperone <sdperone97@gmail.com>
2020-11-30 16:14:18 -0300

    obtención de un gif aleatorio por fetch

--- js/index.js
+++ js/index.js
@@ -2,6 +2,16 @@ window.onload = function () {
     const API_KEY = '7AMJPxtbZk8CRJ20F5S3DO2EFlNHtIrq'
     fetch(`https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=&rating=g`)
         .then((response) => {return response.json()})
-        .then((data) => console.log(data))
+        .then((data) => {
+            // console.log(data.data);
+            let gifName = data.data.title
+            let gifUrl = data.data.image_url
+            let h1 = document.querySelector('h1');
+            let img = document.querySelector('img');
+            h1.innerHTML = gifName ? gifName : "Sin Titulo";
+            img.src = gifUrl
+
+        })
         .catch((err) => console.log('Error: ' + err));
+    
 }
